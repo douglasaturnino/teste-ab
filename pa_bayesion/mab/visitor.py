ce911340677ee2170c6454ced4dfa8204573b803
Douglas Saturnino <douglasaturnino@gmail.com>
2023-07-05 14:52:38 -0300

    Teste A/B bayesiano + MAB

---
+++ pa_bayesion/mab/visitor.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import numpy as np
+import time
+
+driver = webdriver.Chrome()
+
+driver.get('http://127.0.0.1:5000/home')
+
+clicks = 10000
+
+for click in range(clicks):
+    button_color = driver.find_element('name', 'yescheckbox').get_attribute('value')
+
+    if button_color == 'blue':
+
+        if np.random.random() <= 0.30:
+            time.sleep(1)
+            driver.find_element('name', 'yescheckbox').click()
+            driver.find_element('id', 'yesbtn').click()
+
+        else:
+            time.sleep(1)
+            driver.find_element('name', 'nocheckbox').click()
+            driver.find_element('id', 'nobtn').click()
+    else:
+        if np.random.random() <= 0.32:
+            time.sleep(1)
+            driver.find_element('name', 'yescheckbox').click()
+            driver.find_element('id', 'yesbtn').click()
+        else:
+            time.sleep(1)
+            driver.find_element('name', 'nocheckbox').click()
+            driver.find_element('id', 'nobtn').click()
+    
+    #time.sleep(0.2)   
