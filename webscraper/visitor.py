01c20ae663364d5a16c9f5db4d5814cc927b0e08
Douglas Saturnino <douglasaturnino@gmail.com>
2023-07-30 13:32:47 -0300

    Criada a pagina do webcraper.py

--- webscraper/visitor.py
+++ webscraper/visitor.py
@@ -3,35 +3,39 @@ import time
 import numpy as np
 from selenium import webdriver
 
+class visitor:
+  def visitor():
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
 
-driver = webdriver.Chrome(options=options)
-url = os.getenv('WEB_URL')
-driver.get(url)
+    driver = webdriver.Chrome(options=options)
+    url = os.getenv('WEB_URL')
+    driver.get(url)
 
-clicks = 1000
+    clicks = 1000
 
-for click in range(clicks):
-    button_color = driver.find_element('name', 'forwardbtn').get_attribute('value')
+    for click in range(clicks):
+        button_color = driver.find_element('name', 'forwardbtn').get_attribute('value')
 
-    if button_color == 'blue':
+        if button_color == 'blue':
 
-        if np.random.random() <= 0.30:
-            driver.find_element('name', 'yescheckbox').click()
-            driver.find_element('id', 'yesbtn').click()
+            if np.random.random() <= 0.30:
+                driver.find_element('name', 'yescheckbox').click()
+                driver.find_element('id', 'yesbtn').click()
 
+            else:
+                driver.find_element('name', 'nocheckbox').click()
+                driver.find_element('id', 'nobtn').click()
         else:
-            driver.find_element('name', 'nocheckbox').click()
-            driver.find_element('id', 'nobtn').click()
-    else:
-        if np.random.random() <= 0.32:
-            driver.find_element('name', 'yescheckbox').click()
-            driver.find_element('id', 'yesbtn').click()
-        else:
-            driver.find_element('name', 'nocheckbox').click()
-            driver.find_element('id', 'nobtn').click()
-    
-    time.sleep(0.2)   
+            if np.random.random() <= 0.32:
+                driver.find_element('name', 'yescheckbox').click()
+                driver.find_element('id', 'yesbtn').click()
+            else:
+                driver.find_element('name', 'nocheckbox').click()
+                driver.find_element('id', 'nobtn').click()
+        
+        time.sleep(0.2)
+
+    return None
